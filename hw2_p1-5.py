b33f810b8d8a391411d7b508ca15ff5dde98950f
mellamochiao <chou6855@gmail.com>
2024-03-18 23:56:40 +0800

    first commit

---
+++ hw2_p1-5.py
@@ -0,0 +1,17 @@
+print('(a)')
+x=7
+y=8
+if x < 7 or x <= 10 and y > 8:
+   print("ugh")
+else:
+   print("yuck")
+
+print('(c)')
+if 'alpha' < 'zebra':
+   print('alpha < zebra')
+elif 'alpha' > 'zebra':
+   print('alpha > zebra')
+elif 'alpha' == 'zebra':
+   print('alpha == zebra')
+else:
+   print('none of the above')
